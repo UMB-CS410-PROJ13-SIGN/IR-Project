c2fb863e57f53d2953dc80de42f0cf9ba543fb73
admidambrosio <117242179+admidambrosio@users.noreply.github.com>
2022-12-21 17:23:44 -0500

    Update IRSensor.py
    Added more comments to pico code

--- pico_project/IRSensor.py
+++ pico_project/IRSensor.py
@@ -1,16 +1,30 @@
+
+#Author: Adam D'Ambrosio
+#Date: 12/21/2022
+#
+#The following code is intended for use with a Raspberry Pi Pico W
+#that passes the provided tests 1-5. It outputs a signal through pin
+#16 and receives a signal through pin 0. Running the code will power an
+#IR LED for ten seconds. If the IR sensor receives a signal the program
+#will cease and a "true" value will be generated. If no signal is detected
+#within ten seconds a "false" value will be generated. The terminal will
+#then display a prompt asking if the user would like to send the value to
+#the atsign server. Typing 'y' will send the value.
+
+
 def main():
     import time
     from machine import Pin
     led = Pin(16, Pin.OUT) # Output signal to the IR LED
     sensor = Pin(0,Pin.IN) # Input signal from the IR receiver
-    received = False
+    received = False # Value defaults to false
 
     led.value(1)
-    for i in range(100):
+    for i in range(100): # Program runs for about ten seconds
         print("Emitting...")
         time.sleep(.1)
         if sensor.value() == 0: # The sensor returns 0 when an IR signal is received
-            received = True
+            received = True # Set value to true
             print("Signal Received")
             break
 
@@ -20,7 +34,7 @@ def main():
 
 
     import sys
-    shouldRun = str(input('Send? (y/n): ')).lower()
+    shouldRun = str(input('Send? (y/n): ')).lower() # Option to send the value to the atsign server
     if shouldRun != 'y':
         sys.exit(1)
     del sys
